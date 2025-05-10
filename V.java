2c7966d169f09416c4d7ff78d7295bf542354382
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2025-05-09 22:50:17 -0400

    Create V.java

---
+++ V.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class JavaJDBC {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/users", "root", "NEW_PASSWORD"
+            );
+            Statement statement = connection.createStatement();
+
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter password");
+            String password_attempt = myObj.nextLine();
+            ResultSet resultSet = statement.executeQuery("select * from employees where Password = " + "\'" + password_attempt + "\'");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("user_id");
+                String first_name = resultSet.getString("first_name");
+                System.out.println("ID: " + id + " " + "first_name: " + first_name);
+    
+            }
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
